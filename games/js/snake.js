b7dcaef6c310cf0d8d3bb6994cf051865d02c8ca
noodleWrecker7 <hodgkinson.adam@gmail.com>
2018-10-31 20:13:43 +0000

    MWAHA I FIXED COOKIE

--- games/js/snake.js
+++ games/js/snake.js
@@ -29,12 +29,11 @@ window.onload = function () {
     document.addEventListener("keydown", keyPush);
     initialiseGrid();
     generateApple();
-    topScore = getCookie("topScore");
+    topScore = getCookie("highScore");
     setInterval(function () {
         if (!showingEnd) {
             moveEverything();
             drawEverything();
-            topScore = getCookie("topScore");
             scoreHTMl.innerText = "Score: " + (tailLength - 2);
             topScoreHTML.innerHTML = "High score: " + (topScore);
         } else {
@@ -214,18 +213,7 @@ function setCookie(cname, cvalue, exdays) {
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
+function getCookie(a) {
+    var b = document.cookie.match('(^|;)\\s*' + a + '\\s*=\\s*([^;]+)');
+    return b ? b.pop() : '';
 }
