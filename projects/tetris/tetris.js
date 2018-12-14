d2e3bcfb97e1b29a0d640c678b253e6fd5452fe3
noodleWrecker7 <hodgkinson.adam@gmail.com>
2018-12-14 22:48:57 +0000

    moved tetris to own folder to stop it linting from snake

--- projects/tetris/tetris.js
+++ projects/tetris/tetris.js
@@ -4,12 +4,18 @@ const tileGap = 2;
 const tileWidth = 40;
 const columns = 10;
 const rows = 20;
-
+const fps = 25;
 
 window.onload = function () {
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, cvs.width, cvs.height);
+ /*   ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, cvs.width, cvs.height);*/
     const grid = new Grid(columns, rows, tileWidth, tileGap);
+    grid.generate();
+    grid.draw();
+
+    //let testShape = [{x:}]
+
+    let s = new Shape();
 
     document.addEventListener("keydown", keyPush);
 
@@ -20,37 +26,56 @@ window.onload = function () {
 
 class Grid {
 
-
-
     constructor(columns, rows, tileWidth, tileGap) {
         this.columns = columns;
         this.rows = rows;
-        this.tileWidth = tileWidth;
-        this.tileGap = tileGap;
+        this.tWidth = tileWidth;
+        this.tGap = tileGap;
         this.gridArray = [];
     }
 
     generate() {
-        for (let c = 0; i < columns; i++) {
+        for (let c = 0; c < columns; c++) {
             this.gridArray[c] = [];
-            for (let r = 0; i < rows; i++) {
-                gridArray[c][r] = {}
+            for (let r = 0; r < rows; r++) {
+                this.gridArray[c][r] = {
+                    realX: tileGap + c * (tileGap + tileWidth),
+                    realY: tileGap + r * (tileGap + tileWidth),
+                    colour: "black"
+                };
             }
         }
     }
+
+    draw(){
+        for (let c = 0; c < columns; c++) {
+            for (let r = 0; r < rows; r++) {
+                let gS = this.gridArray[c][r];
+                ctx.fillStyle = gS.colour;
+                ctx.fillRect(gS.realX, gS.realY, tileWidth, tileWidth);
+            }
+        }
+    }
+
+
 }
 
 class Shape {
 
-    constructor(count){ // count of blocks
-        this.count = count;
+    //blockArray;
+
+    constructor(bArr) { // count of blocks
+
     }
 
+}
+
+class Cyan extends Shape {
 
 }
 
 function keyPush(e) {
-    id = e.key;
+    let id = e.key;
 
     if (['ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown'].indexOf(id) > -1) { // if id is in list
         e.preventDefault(); // stop scroll
